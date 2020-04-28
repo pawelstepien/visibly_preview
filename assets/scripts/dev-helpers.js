e131ebd1b5f912d257ad41bf1719984731c32142
pawelstepien <pawelstepien1000@gmail.com>
2020-04-28 12:06:21 +0200

    Fixed decorative rects on mobile

--- assets/scripts/dev-helpers.js
+++
@@ -1,7 +0,0 @@
-const initDevHelpers = () => {
-    const lockScrollTrigger = document.getElementById('lock-scroll-trigger');
-    lockScrollTrigger.addEventListener('click', () => {
-        document.body.classList.toggle('lock-scroll');
-    });
-}
-export {initDevHelpers}
